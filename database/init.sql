0d07469ed3e819120180a7063062947ff410138c
Bruno-nog <bqueiroz.nog@gmail.com>
2026-05-18 19:08:34 -0300

    creating db

---
+++ database/init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username TEXT NOT NULL,
+  email TEXT NOT NULL,
+  hashed_password TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE media (
+  id SERIAL PRIMARY KEY,
+  external_id TEXT NOT NULL,
+  media_type TEXT NOT NULL,
+  title TEXT NOT NULL,
+  description TEXT,
+  cover_url TEXT,
+  release_year INT,
+  genres TEXT,
+  created_at TIMESTAMP DEFAULT NOW()
+);
