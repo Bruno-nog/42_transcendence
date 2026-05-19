d5b4552a3886ece11b2da4d3ec5e887e7eae960d
Bruno-nog <bqueiroz.nog@gmail.com>
2026-05-19 17:02:03 -0300

    request for a movie from an api is working

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = FastAPI()
+tmdb_url = "https://api.themoviedb.org/3"
+tmdb_api = os.getenv("TMDB_API_KEY")
 
-@app.get("/health")
-def health():
-    return {"status": "ok"}
+@app.get("/search")
+def route_search(title: str):
+    response = requests.get(tmdb_url + "/search/movie", params={"api_key": tmdb_api, "query": title, "language": "pt-BR"})
+    return response.json()["results"]
